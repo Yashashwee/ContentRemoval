0a01a2379f439eb40b36a3a05e115e27af2073fa
Shuvam Manna <shuvam.manna@gmail.com>
2019-05-02 19:59:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var imported = document.createElement('script');
+imported.src = 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js';
+document.head.appendChild(imported);
 
 let filenames = [
   "pepe1.jpg",
@@ -10,6 +13,15 @@ for (let imgElt of imgs) {
   let r = Math.floor(Math.random() * filenames.length);
   let file = 'images/' + filenames[r];
   let url = chrome.extension.getURL(file);
+
+  for(var i=0; i<imgElt.length; i++){
+  img = loadImage(imgElt[i]);
+
+  //Send POST request
+
+  if(1){
   imgElt.src = url;
   console.log(url);
+  }
+}
 }
